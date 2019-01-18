a478244e62781ec9e53b8dba1cb24c8b1b837f2f
Chaitanya Gangavarapu <chaitanya.gangavarapu@gmail.com>
2019-01-17 17:55:51 -0800

    finished everything but math ops

--- ComplexCalc/ComplexCalc/Calculator.swift
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -10,4 +10,62 @@ import Foundation
 
 // All your work will go in here
 class Calculator {
+    
+    func add(lhs: Int, rhs: Int) -> Int{
+        return lhs+rhs;
+    }
+    
+    func subtract(lhs: Int, rhs: Int) -> Int{
+        return lhs-rhs;
+    }
+    
+    func multiply(lhs: Int, rhs: Int) -> Int{
+        return lhs*rhs;
+    }
+    
+    func divide(lhs: Int, rhs: Int) -> Int{
+        return lhs/rhs;
+    }
+    
+    func add(_ args: [Int]) -> Int {
+        var sum: Int = 0;
+        for element in args {
+            sum += element;
+        }
+        return sum;
+    }
+    
+    func multiply(_ args: [Int]) -> Int {
+        var product: Int = 1;
+        for element in args {
+            product = product * element;
+        }
+        return product;
+    }
+    
+    func count(_ args: [Int]) -> Int {
+        return args.count;
+    }
+    
+    func avg(_ args: [Int]) -> Int {
+        return add(args)/count(args);
+    }
+    
+    func add(lhs: [String: Int], rhs: [String: Int]) -> [String: Int] {
+        return ["x": lhs["x"]!+rhs["x"]!, "y": lhs["y"]! + rhs["y"]!]
+    }
+    
+    func subtract(lhs: [String: Int], rhs: [String: Int]) -> [String: Int] {
+        return ["x": lhs["x"]!-rhs["x"]!, "y": lhs["y"]! - rhs["y"]!]
+    }
+    
+    func add(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
+        return (lhs.0 + rhs.0, lhs.1 + rhs.1)
+    }
+    
+    func subtract(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
+        return (lhs.0 - rhs.0, lhs.1 - rhs.1)
+    }
 }
+
+
